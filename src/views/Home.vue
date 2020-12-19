145d1f87c9088982c5fd4a15b89c938c09b84126
udream-Wangzh <wangzh@udream.cn>
2020-12-19 18:16:29 +0800

    fix: 初始化项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="screenShot">点击截屏</button>
+    <div class="screent-wrap">
+      <h1>1</h1>
+      <h1>2</h1>
+      <h1>3</h1>
+    </div>
+
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="60%">
+      <div ref="imageWrap"></div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import html2canvas from "html2canvas";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data: function() {
+    return {
+      dialogVisible: false
+    };
+  },
+  methods: {
+    screenShot() {
+      html2canvas(document.querySelector(".screent-wrap")).then(canvas => {
+        this.dialogVisible = true;
+        this.$nextTick(() => {
+          this.$refs.imageWrap.appendChild(canvas);
+        });
+      });
+    }
   }
 };
 </script>
+<style lang="scss" scoped>
+.screent-wrap {
+  width: 750px;
+  height: 1000px;
+  margin: 0 auto;
+  background: red;
+}
+</style>
